3853827d0baf456337cd3d0f7a9011cbf0d772f5
=zwileo <=leo.zwilling@orange.fr>
2023-03-24 14:34:30 +0100

    ajout tableau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,36 @@ export class AppComponent {
   verif:boolean=false;
 
   element:number=1
+
+  etudiants:any[] = [
+    {"nom":"Dupont", "age":25},
+    {"nom":"caduque", "age":30},
+    {"nom":"Toto", "age":22}
+]
+
+  choixClasse(isFirst:boolean, isLast:boolean)
+  {
+    var ma_classe:string="";
+    if(isFirst || isLast)
+    {
+      ma_classe = "first_last";
+    }
+    return ma_classe;
+  }
+
+  choixClasse2(isEven:boolean)
+  {
+    var ma_classe:string="";
+    if(isEven)
+    {
+      ma_classe = "even"
+    }
+    else
+    {
+      ma_classe = "odd"
+    }
+    return ma_classe;
+  }
+
+  
 }
